b6d31188eb80f5724b855aedf60b1aed36d039f4
G186742021 <218674@st.takushoku-u.ac.jp>
2022-07-28 14:18:38 +0900

    7/28

--- kazuate/kazuate.js
+++ kazuate/kazuate.js
@@ -1,29 +1,32 @@
 let kotae =Math.floor(Math.random()*10)+1;
 console.log('答え:'+kotae);
 
-let c = document.querySelector('#print');
-c.addEventListener('click',hantei)
 let kaisu=0;
 
-for(let i of 4){
-hantei();
-kaisu++;
-}
+let c = document.querySelector('#print');
+c.addEventListener('click',hantei)
+
 
 function hantei (){
     let l=document.querySelector('input[name="seisu"]');
-    let kazuate=l.value;
-   let yoso=Number(kazuate);
-let result =document.querySelector('#result')
-let p=document.createElement('p');
-let s =docement.querySelector('span#kaisu');
-s.textContent=kaisu;
-let a=document.querySelector('div#seisu');
-a.textContent=yoso;
+    let d=l.value;
+    let yoso=Number(d);
+
+    let result =document.querySelector('#result');
+    let p=document.createElement('p');
+     kaisu++;
+    let s =docement.querySelector('span#kaisu');
+     s.textContent=kaisu;
+    let a=document.querySelector('div#seisu');
+     a.textContent=yoso;
 
     if(kazuate==kotae){
-        console.log('正解です。おめでとう！');
-    }else{
-        console.log('間違い');
-    } 
+        result.texstContent="正解です。おめでとう！";
+    }else if(kaisu>3){
+        result.textContent="ざんねん答えは"+kotae+"でした。";
+    }else if(kotae>yoso){
+        result.textContent="答えはもっと小さいです。";
+    }else if(kotae<yoso){
+        result.textContent="答えはもっと大きいです。";
+    }
 }
