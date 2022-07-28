a478d04f90087f1e3eee10020106d08f8d9334e2
G186742021 <218674@st.takushoku-u.ac.jp>
2022-07-28 15:27:17 +0900

    7/28

--- kazuate/kazuate.js
+++ kazuate/kazuate.js
@@ -20,10 +20,12 @@ function hantei (){
     let a=document.querySelector('div#seisu');
      a.textContent=yoso;
 
-    if(kazuate==kotae){
+    if(kotae==yoso){
         result.texstContent="正解です。おめでとう！";
-    }else if(kaisu>3){
+    }else if(kaisu==3){
         result.textContent="ざんねん答えは"+kotae+"でした。";
+    }else if(kaisu>3){
+        result.textContent="答えは"+kotae+"でした。すでにゲームは終了しています。";
     }else if(kotae>yoso){
         result.textContent="答えはもっと小さいです。";
     }else if(kotae<yoso){
